beb8ca0cefa20621f6448d5f76db900eab076ba2
antorrg <114789901+antorrg@users.noreply.github.com>
2024-06-28 19:16:25 -0300

    update envConfig

--- api/envConfig.js
+++ api/envConfig.js
@@ -2,10 +2,10 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 
-const ConnectDb = `postgres://${process.env.VITE_DB_USER}:${process.env.VITE_DB_PASSWORD}@${process.env.VITE_DB_HOST}/${process.env.VITE_DB_NAME}`
+//const ConnectDb = `postgres://${process.env.VITE_DB_USER}:${process.env.VITE_DB_PASSWORD}@${process.env.VITE_DB_HOST}/${process.env.VITE_DB_NAME}`
 //console.log(process.env.VITE_GMAIL_USER)
 export default {
- ConnectDb,
+ ConnectDb: process.env.VITE_VERCEL_DB,
  SecretKey: process.env.VITE_SECRET_KEY,
  userImg: process.env.VITE_USER_IMG,
  gmailUser: process.env.VITE_GMAIL_USER,

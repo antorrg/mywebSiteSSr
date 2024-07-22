a8614ff8b357c51d1e3c1cf3e52da559ee0abebf
antorrg <antoniorodriguezgramajo@gmail.com>
2024-07-22 02:08:05 -0300

    ajustando vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()]
+// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    minify: false, // Esto puede ayudar a depurar en producción
+  },
+  ssr: {
+    noExternal: ['styled-components', 'react-router-dom', 'react-redux'], // Ajusta según tus necesidades
+  },
 })
